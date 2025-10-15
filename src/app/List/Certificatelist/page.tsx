640fa4638eb59a0e706b32d4b198a5344292f2bb
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-10-15 16:13:17 +0530

    update file

--- src/app/List/Certificatelist/page.tsx
+++ src/app/List/Certificatelist/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState, useEffect } from 'react';
 import { db } from '../../lib/firebaseconfig'; // Import Firestore setup
 import { collection, getDocs, doc, deleteDoc, updateDoc } from 'firebase/firestore'; // Firestore functions
